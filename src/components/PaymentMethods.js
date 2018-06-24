c17ca0f9bed66c99d7804c318207eec056fbcf4a
Nicolas Kittsteiner <nicolaskittsteiner@MacBook-Pro-de-Nicolas.local>
2018-06-24 13:33:39 -0400

    Doing the following changes - Handling khipu credentials on localStorage - Refactoring on styles, doing tests on 3 views

--- src/components/PaymentMethods.js
+++ src/components/PaymentMethods.js
@@ -2,57 +2,12 @@ import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import AppBar from 'material-ui/AppBar';
-import {fullBlack, purpleA700, limeA200, white} from 'material-ui/styles/colors';
 import { Link } from "react-router-dom";
 import {GridList, GridTile} from 'material-ui/GridList';
 import UpperMenu from './UpperMenu';
 import Logo from './Logo';
+import { styles, muiTheme } from './styles'
 
-const styles = {
-  root:{
-    display: 'flex',
-    flexWrap: 'no-wrap',
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-    alignItems: 'center'
-  },
-  button: {
-    margin: 12
-  },
-  logo: {
-    width: 50,
-    height: 50
-  },
-  title: {
-    fontSize: 16, 
-    textAlign: 'center' 
-  },
-  gridList: {
-    width: 300,
-    overflowY: 'auto',
-  },
-  tiles: {
-      width: 100,
-      height: 100
-  },
-  menu: {
-      backgroundColor: purpleA700,
-      color: white
-  }
-  
-};
-
-const muiTheme = getMuiTheme({
-  palette: {
-    textColor: fullBlack,
-    primary1Color: purpleA700,
-    accent1Color: limeA200,
-  },
-  appBar: {
-    height: 60,
-    textColor: fullBlack
-  },
-});
 
 const PaymentMethods = ({ match }) => (
   <MuiThemeProvider muiTheme={getMuiTheme(muiTheme)}>
@@ -63,14 +18,12 @@ const PaymentMethods = ({ match }) => (
     <div style={{ height: 20}}></div>
     <GridList
       cellHeight={160}
-      style={styles.gridList}
-    >
+      style={styles.gridList}>
         <Link to="/setupkhipu"><GridTile
           key={''}
-          title={'Configurar'}
-        >
+          title={'Configurar'}>
           <img alt="" style={styles.tiles} src={require('../images/khipu.png')} />
-        </GridTile></Link>        
+        </GridTile></Link>
        
     </GridList>
     </div>
